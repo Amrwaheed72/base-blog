95e7cc4252976e14008e9f7a6af15e59379415b2
Amr Waheed <amrmohamed877@gmail.com>
2025-04-22 19:15:04 +0200

    handling update mutation

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -1,14 +1,13 @@
 import "./PostDetail.css";
 import useFetchComments from "./useFetchComments";
-export function PostDetail({ post,isSuccess, mutate }) {
+export function PostDetail({ post, isSuccess, mutate, update, isUpdated }) {
   // replace with useQuery
   const { id } = post;
   const { data, isPending, error } = useFetchComments(id);
 
-
   if (isPending) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
-console.log(isSuccess)
+  console.log(isSuccess);
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
@@ -16,7 +15,10 @@ console.log(isSuccess)
         <button onClick={() => mutate(id)}>Delete</button>
         {isSuccess && <p>Post Deleted Successfully</p>}
       </div>
-      <button>Update title</button>
+      <div>
+        <button onClick={() => update(id)}>Update title</button>
+        {isUpdated && <p>Post Updated Successfully</p>}
+      </div>
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
